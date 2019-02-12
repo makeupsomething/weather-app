e9fb9c992ca9fe64bed7fca113d07399a39fabdf
daryl <darylcoleoffice@gmail.com>
2019-02-12 21:41:31 +0900

    Get location on home mount

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,41 @@ import React from "react";
 import SearchBar from "./SearchBar";
 
 class Home extends React.Component {
+  state = {
+    latitude: null,
+    longitude: null,
+    error: null,
+  }
+
+  componentDidMount() {
+    this.geoFindMe()
+  }
+
+  success = (position) => {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    this.setState({
+      latitude,
+      longitude
+    })
+  }
+
+  error = () => {
+    this.setState({
+      error: "Unable to retrieve your location"
+    })
+  }
+
+  geoFindMe = () => {
+    if (!navigator.geolocation){
+      this.setState({
+        error: "eolocation is not supported by your browser"
+      })
+      return;
+    }
+    navigator.geolocation.getCurrentPosition(this.success, this.error)
+  }
+
   render() {
     return (
       <div>
