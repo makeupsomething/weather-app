d3653efa7c9c7fa02f297b8a37963c0d2b25ca6a
daryl <darylcoleoffice@gmail.com>
2019-02-12 11:58:35 +0900

    Improve testibility of search bar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { navigate } from "@reach/router";
 import { separateByDay } from "../utils";
 import { getWeatherByCityName } from "../api";
+import { ninvoke } from "q";
 
 const SearchBarForm = styled.form`
   font-size: 2rem;
@@ -16,14 +17,35 @@ const SearchBarInput = styled.input`
   font-size: 2rem;
 `;
 
+const SearchBarButton = styled.button`
+  background-color: darkgray;
+  border: none;
+  margin: 12px;
+  padding: 12px;
+  font-size: 2.5rem;
+`
+
+const ErrorMessage = styled.span`
+  color: red;
+  font-size: 1.5rem;
+`
+
 class SearchBar extends React.Component {
   state = {
-    location: ""
+    location: "",
+    isSaving: false,
+    error: false,
   };
 
   handleSubmit = event => {
     event.preventDefault();
-    navigate(`/results/${this.state.location}`);
+    const {city} = event.target.elements
+    if(city.value) {
+      this.setState({isSaving: true})
+      navigate(`/results/${city.value}`);
+    } else {
+      this.setState({error: "City cannot be empty"})
+    }
   };
 
   handleLocationChange = event => {
@@ -33,17 +55,26 @@ class SearchBar extends React.Component {
   };
 
   render() {
+    const {error} = this.state
+
     return (
-      <SearchBarForm onSubmit={this.handleSubmit} autocomplete="off">
-        <label htmlFor="city">Check the weather in: </label>
-        <SearchBarInput
-          onChange={this.handleLocationChange}
-          type="text"
-          id="city"
-          placeholder="Dublin"
-          value={this.state.location}
-        />
-      </SearchBarForm>
+      <React.Fragment>
+        <SearchBarForm onSubmit={this.handleSubmit}>
+          <label htmlFor="city">Check the weather in: </label>
+          <SearchBarInput
+            required
+            onChange={this.handleLocationChange}
+            type="text"
+            id="city"
+            placeholder="Dublin"
+            value={this.state.location}
+          />
+          <SearchBarButton type="submit" disabled={this.state.isSaving}>
+            submit
+          </SearchBarButton>
+        </SearchBarForm>
+        {error ? <ErrorMessage>{error}</ErrorMessage> : null}
+      </React.Fragment>
     );
   }
 }
